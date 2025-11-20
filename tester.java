7dcb22091f7d4a97079a80ffe3754501ff9eaa6d
katherinecurtis99 <katherinecurtis99@gmail.com>
2025-11-20 15:28:45 +0000

    created Tester.java and added basic form

--- tester.java
+++ tester.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+/**
+ * Tests Classes
+ * Author - Group 11
+ * Version - 1.0
+ */
+
+ public class Tester
+ {
+    public static void main(String[] args)
+    {
+        System.out.println("I hate this module. ");
+    }
+ }
