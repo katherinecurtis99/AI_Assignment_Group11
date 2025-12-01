2e3a1314ed12c5a7e3a4c527885f342f8e210573
katherinecurtis99 <katherinecurtis99@gmail.com>
2025-12-01 19:28:52 +0000

    applied lawmower image

--- agent/lawnMower.java
+++ agent/lawnMower.java
@@ -2,6 +2,11 @@ package agent;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+
 
 import main.GamePanel;
 import main.DirectionHandler;
@@ -10,14 +15,33 @@ public class lawnMower extends entity
 {
 	GamePanel gp;
 	DirectionHandler dh;
+	private BufferedImage image;
 	
   public lawnMower(GamePanel gp, DirectionHandler dh) 
 	{
 		this.gp = gp;
 		this.dh = dh;
+		loadImage("/res/lawn_mower.png");
 		setDefaultValues();
 	}
 	
+
+  private void loadImage(String resourcePath) {
+	try (InputStream is = getClass().getResourceAsStream(resourcePath)) {
+		if (is ==null) {
+			System.err.println("Resource not found" + resourcePath);
+			image = null;
+			return;
+		}
+		image = ImageIO.read(is);
+	}
+	catch (IOException e) {
+		e.printStackTrace();
+		image = null;
+	}
+
+  }
+
   public void setDefaultValues()
 	{
 		x     = 100;
@@ -47,7 +71,13 @@ public class lawnMower extends entity
 	
   public void draw(Graphics2D g2)
 	{
+		if (image != null) {
+			g2.drawImage(image, x, y, gp.tileSize, gp.tileSize,null);
+			return;
+		}
+		else{
 		g2.setColor(Color.white);
 		g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+		}
 	}
 }
